4a7351499f62938fcd05bc34393e4ce3509f2dc3
leveille <leveillej@gmail.com>
2009-07-07 11:45:29 -0400

    Added separate utils js file
    Updated modules

--- 960-full/assets/javascripts/application.js
+++ 960-full/assets/javascripts/application.js
@@ -1,61 +1,17 @@
 //set up a namespace for wrapping up methods and properties (public and private)
 //http://yuiblog.com/blog/2007/06/12/module-pattern/
-BLUE = {};
-BLUE.app = function()
-{
+if(!this.BLUE) {
+    BLUE = {};
+}
+BLUE.app = (function() {
     return {
         init: function()
         {
-            
+            BLUE.utils.getCookie('bar');
         }
     };
-    
-}();
+})();
 
 $(document).ready(function() {
     BLUE.app.init();
 });
-
-BLUE.utils = function()
-{
-    return {
-        
-        getCookie: function(name)
-        {
-            var start = document.cookie.indexOf(name + "=");
-            var len = start + name.length + 1;
-            if ((!start) && (name != document.cookie.substring(0, name.length))) {
-                return null;
-            }
-            if (start == -1) return null;
-            var end = document.cookie.indexOf(';', len);
-            if (end == -1) end = document.cookie.length;
-            return unescape(document.cookie.substring(len, end));
-        },
-        
-        setCookie: function(name, value, expires, path, domain, secure)
-        {
-            var today = new Date();
-            today.setTime(today.getTime());
-            if (expires) {
-                expires = expires * 1000 * 60 * 60 * 24;
-            }
-            var expires_date = new Date(today.getTime() + (expires));
-            document.cookie = name + '=' + escape(value) +
-                ((expires) ? ';expires=' + expires_date.toGMTString() : '') + //expires.toGMTString()
-                ((path) ? ';path=' + path : '') +
-                ((domain) ? ';domain=' + domain : '') +
-                ((secure) ? ';secure' : '');
-        },
-        
-        deleteCookie: function(name, path, domain)
-        {
-            if (BAI.getCookie(name)) 
-                document.cookie = name + '=' +
-                ((path) ? ';path=' + path : '') +
-                ((domain) ? ';domain=' + domain : '') +
-                ';expires=Thu, 01-Jan-1970 00:00:01 GMT';
-        }
-    
-    };
-}
